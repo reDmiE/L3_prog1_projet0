2f7cfec2f5b68b3a254ba9583ef36b5853db6e4f
reDmiE <remi.piau@ens-rennes.fr>
2018-09-20 21:34:28 +0200

    make scripts better for hanoi and add one for penrose

--- penrose/run_penrose.sh
+++ penrose/run_penrose.sh
@@ -2,11 +2,7 @@
 if [ $# -eq 1 ]
 then
   make
-  ocaml hanoi_r.ml
-  R -f times.r
-  mkdir -p pdf
-  mv times.pdf pdf/times.pdf
-  ocaml hanoi_ext.ml $1
+  ocaml penrose_base.ml $1
   make clean
 else
   echo "Usage : ./run_hanoi.sh <number_of_discs>"
